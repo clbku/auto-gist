2460739c0198d570674dca31253968e97420cb59
Ly Hoang <lyhc@gisx.vn>
2023-08-10 14:58:25 +0700

    feat(GitGraph): remove

--- app/components/App.tsx
+++ app/components/App.tsx
@@ -1,10 +1,7 @@
-import React from 'react';
-
 import "./App.css";
 
 import { ConventionCommit } from './ConventionCommit/ConventionCommit';
 import { AutoVersioning } from "./AutoVersioning/AutoVersioning";
-import { GitGraph } from './GitGraph/GitGraph';
 
 export const App = () => {
 
@@ -14,8 +11,6 @@ export const App = () => {
         return <AutoVersioning />;
       case "convention-commit":
         return <ConventionCommit />;
-      case "git-graph":
-        return <GitGraph />;
     }
   };
 
